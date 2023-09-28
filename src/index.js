3aadde9a79ed1d39f2459434b67274ba22549bec
Maheshkumahm <maheshkumahm153@gmail.com>
2023-09-28 12:24:23 +0530

    280923

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import login from './login';
 import Studentdata from './components/Studentdata';
 import backgroundImage from './STUDENTS.jpeg';
+import Login from './login';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Studentdata />
+    < Login />
+
   </React.StrictMode>
 );
 
